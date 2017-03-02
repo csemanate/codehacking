0ed1a7e8336b859dc2ac0ac90a0d4118c9538c67
Carlos Semanate <csemanate@mejoracontinua.biz>
2017-03-02 23:33:06 +0000

    users with photos working

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -11,7 +11,7 @@
         </div>
         <div class="form-group">
             {!! Form::label('email', 'Email:') !!}
-            {!! Form::email('email', null, ['class'=>'form-control']) !!}
+            {!! Form::email('email',null, ['class'=>'form-control']) !!}
         </div>
         <div class="form-group">
             {!! Form::label('role_id', 'Role:') !!}
@@ -22,8 +22,8 @@
             {!! Form::select('is_active', array(1=>'Active', 0=>'Not Active'), 0, ['class'=>'form-control']) !!}
         </div>
         <div class="form-group">
-            {!! Form::label('file', 'Title:') !!}
-            {!! Form::file('file', null, ['class'=>'form-control']) !!}
+            {!! Form::label('photo_id', 'Title:') !!}
+            {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
         </div>
         <div class="form-group">
             {!! Form::label('password', 'Password:') !!}
