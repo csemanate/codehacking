9ef2bd84249f166b5fda5e7a6468e12e11d02f8c
Carlos Semanate <csemanate@mejoracontinua.biz>
2017-03-06 06:06:03 +0000

    Finished users

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -10,6 +10,7 @@ use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Session;
 
 class AdminUsersController extends Controller
 {
@@ -68,6 +69,8 @@ class AdminUsersController extends Controller
 
         User::create($input);
 
+        Session::flash('created_user', 'The user has been created.');
+
         return redirect('/admin/users');
 
     }
@@ -125,6 +128,7 @@ class AdminUsersController extends Controller
         }
 
         $user->update($input);
+        Session::flash('updated_user', 'The user has been updated.');
         return redirect('/admin/users');
     }
 
@@ -138,5 +142,14 @@ class AdminUsersController extends Controller
     public function destroy($id)
     {
         //
+        $user = User::findOrFail($id);
+
+        unlink(public_path() . $user->photo->file);
+
+        $user->delete();
+
+        Session::flash('deleted_user', 'The user has been deleted.');
+
+        return redirect ('/admin/users');
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,3 +1,7 @@
 @extends('layouts.admin')
 
-@yield('content')
+@section('content')
+
+<h1>Admin</h1>
+
+@endsection
